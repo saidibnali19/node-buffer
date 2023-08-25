c2ac91cbdf1f6598ef1bd11e8514260d3bde699e
Said Ali <saidibnali19@gmail.com>
2023-08-25 16:53:35 +0300

    feat: learnt about buffers and character encodings

---
+++ Buffers and character encodings/app.js
@@ -0,0 +1,16 @@
+import { Buffer } from "node:buffer";
+
+const buffer = Buffer.from("hello world", "utf8");
+// console.log(buffer.toString("hex"));
+
+// console.log(buffer.toString("base64"));
+
+// console.log(Buffer.from("this thing", "utf8"));
+
+// console.log(Buffer.from("this thing", "utf16le"));
+
+// console.log(Buffer.from("1ag123", "hex"));
+
+// console.log(Buffer.from("1a7", "hex"));
+
+console.log(Buffer.from("1634", "hex"));
